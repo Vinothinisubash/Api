e598a3099ffc962110d20b10cd0981c926146248
Vinothinisubash <Vinothini.s1711@gmail.com>
2023-06-16 15:12:42 +0530

    first

---
+++ src/main/java/com/req/APIreq/App.java
@@ -0,0 +1,13 @@
+package com.req.APIreq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
